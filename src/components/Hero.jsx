32849adf95725b5e5f4aee489d1a196eda1ffc45
Kevin Roshan N <kevinroshann@gmail.com>
2025-03-20 00:19:54 +0530

    add marquee

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,12 +15,11 @@ const Hero = () => {
       className={cn(
         "absolute inset-0",
         "[background-size:40px_40px]",
-        "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
-        "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+        "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]"
       )} />
     {/* Radial gradient for the container to give a faded look */}
     <div
-      className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] "></div>
 <div className="flex flex-col items-center">
   <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-5xl font-bold text-black sm:text-6xl md:text-8xl lg:text-9xl xl:text-10xl">
    VIBHAVA
